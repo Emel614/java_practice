5412436b2acc8648a305d21799ed6ed206aa416a
Emel614 <emelny614@gmail.com>
2021-08-09 15:10:04 +0200

    First Question

---
+++ src/day01/Question01.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class Question01 {
+    /*
+     * Write method that accepts a String and extracts letters and numbers.
+     *  Method should return a String.
+     *  EX: a1b2c3 --> Letters are abc and numbers are 123.
+     */
+}
