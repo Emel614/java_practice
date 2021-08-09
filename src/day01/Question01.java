a421f79055ee92ac9638ea5ef3430c38428f4f6f
Emel614 <emelny614@gmail.com>
2021-08-09 15:23:38 +0200

    First Question

--- src/day01/Question01.java
+++ src/day01/Question01.java
@@ -6,4 +6,23 @@ public class Question01 {
      *  Method should return a String.
      *  EX: a1b2c3 --> Letters are abc and numbers are 123.
      */
+    public static void main(String[] args) {
+        String str="a1b2c3";
+        extractLetterAndNumber(str);
+    }
+
+    public static void extractLetterAndNumber(String str) {
+        String letter = "";
+        String number = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i))) {
+                letter += str.charAt(i);
+            }
+            if (Character.isDigit(str.charAt(i))) {
+                number += str.charAt(i);
+            }
+        }
+        System.out.println("Letters are= " + letter);
+        System.out.println("Numbers are= " + number);
+    }
 }
